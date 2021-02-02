90ef1135f974687ecf91d5fb50c22cc89dce343a
zakordonetss <60739399+zakordonetss@users.noreply.github.com>
2021-02-02 21:58:28 +0200

    tried making a request to the server

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+    public posts;
+
+    constructor(public http: HttpClient) {}
+
+    ngOnInit() {
+        this.http.get('Access-Control-Allow-Origin: https://amazing.site')
+            .subscribe(posts => {
+                this.posts = posts;
+                console.log(posts);
+            })
+    }
 }
