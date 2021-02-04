0ed30d795ea22f1e4ebcfd3bd32e5fdddf15b6cf
zakordonetss <60739399+zakordonetss@users.noreply.github.com>
2021-02-04 20:58:14 +0200

    tried to buil a table

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,24 +1,44 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
+export interface Post {
+    publicationType: string,
+    termType: string,
+    reportGroup: string,
+    reportState: string,
+    reportFormat: string,
+    outputDate: {
+        date: Date,
+    },
+    outputNumber: number,
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent implements OnInit {
-    public posts;
+    public posts = [];
+    public data;
 
     constructor(public http: HttpClient) {}
 
     ngOnInit() {
         this.http.get('http://localhost:4050/test-data')
-            .subscribe(posts => {
-                this.posts = posts;
+            .subscribe(data => {
+                this.data = data;
             })
+        
+        for (let post of this.data) {
+            this.posts.push(post)
+        }
     }
 
     log() {
         console.log(this.posts);
+        console.log(this.data);
     }
+
+    
 }
